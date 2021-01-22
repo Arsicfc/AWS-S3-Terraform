2fc45636ab53536ebbd8bc7d361b2132d62e3422
Arsalan Siddiqui <arsi_sidd@hotmail.com>
2021-01-22 05:04:02 -0800

    Update site.tf

--- site.tf
+++ site.tf
@@ -14,3 +14,15 @@ resource "aws_s3_bucket" "site" {
  }
 }
 
+# Create S3 Object
+resource "aws_s3_bucket_object" "object" {
+  bucket = aws_s3_bucket.site.bucket
+  key    = "<fileName>.html"
+  source = "<folderLocation/fileName.html>"
+  acl    = "public-read"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("<folderLocation/fileName.html>")
+}
